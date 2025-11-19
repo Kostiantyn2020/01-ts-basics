f424700741f23835bba230725fb9a18b9c90c885
Kostiantyn2020 <161701157+Kostiantyn2020@users.noreply.github.com>
2025-11-19 15:13:00 +0100

    1.2

--- vite-project/src/task-2.ts
+++ vite-project/src/task-2.ts
@@ -1,10 +1,10 @@
-interface Product {
+interface product {
   readonly id: number; // тільки для читання
   title: string;
   description?: string; // необов'язкове поле
 }
 
-const product: Product = {
+const product: product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
